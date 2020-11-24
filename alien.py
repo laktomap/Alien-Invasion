03f28ea9ca8c251cb17e8bd2d4c4206b9f46cdd3
laktomap <laktomap@protonmail.com>
2020-11-24 18:09:56 +0100

    Test 2 eingefügt

--- alien.py
+++ alien.py
@@ -14,6 +14,8 @@ class Alien(Sprite):
         self.image = pygame.image.load('images/alien.bmp')
         self.rect = self.image.get_rect()
 
+        #Test2
+
         # Start each new alien near the top left of the screen.
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
